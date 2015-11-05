e414511dc3b9aa739fc21bea90ab64facfb01a2d
Hao Zhu <haozhu@hsl.harvard.edu>
2015-11-05 17:52:59 -0500

    initial commit

---
+++ R/test-add_footnote.r
@@ -0,0 +1,11 @@
+context("Add_Footnote")
+#####
+
+rtable <- data.frame(variable = c("mpg", "wt"), mean = c(20.091, 3.217), sd = c(6.027, 0.978))
+htmltable_1 <- kable(rtable, format = "html")
+htmltable_2 <- kable(rtable, format = "html", caption = "Table", table.attr = "class = 'table table-striped table-hover'")
+latextable_1 <- kable(rtable, format = "latex")
+latextable_2 <- kable(rtable, format = "latex", caption = "Table", row.names = T, align = c("l", "c", "c"))
+latextable_3 <- kable(rtable, format = "latex", booktab = T)
+latextable_4 <- kable(rtable, format = "latex", booktab = T, caption = "Table")
+
---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+library(knitr)
+library(kableExtra)
+library(testthat)
+
+test_check("kableExtra")
