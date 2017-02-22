981933bb61b34cf95fdc87cf7cb108fda438d150
Hao Zhu <haozhu233@gmail.com>
2017-02-22 18:46:23 -0500

    quick demo

--- R/kableExtra-package.R
+++ R/kableExtra-package.R
@@ -2,6 +2,7 @@
 #'
 #' @importFrom stringr str_count
 #' @importFrom xml2 read_xml xml_attr xml_has_attr xml_attr<- read_html
+#' xml_child xml_children xml_name xml_add_sibling xml_add_child
 #' @importFrom rvest html_table
 #' @importFrom knitr kable
 #' @name kableExtra-package
