68d4fe9e3fdd290c4c34b708f3174d574d7650a0
Hao Zhu <haozhu233@gmail.com>
2017-07-25 14:07:50 -0400

    Fixed #29

--- R/kable_styling.R
+++ R/kable_styling.R
@@ -269,7 +269,7 @@ styling_latex_repeat_header <- function(x, table_info, repeat_header_text,
       caption_without_lab <- sub("\\\\label\\{[^\\}]*\\}", "", table_info$caption)
       repeat_header_text <- paste(caption_without_lab, repeat_header_text)
     }
-    continue_line <- paste0("\\caption{", repeat_header_text, "}\\\\")
+    continue_line <- paste0("\\caption[]{", repeat_header_text, "}\\\\")
   }
 
   x <- c(
