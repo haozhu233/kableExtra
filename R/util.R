f451dbb62cf72e16050fe39ea5ae99b3fd53a617
Hao Zhu <haozhu233@gmail.com>
2017-08-22 16:51:38 -0400

    Fix #44

--- R/util.R
+++ R/util.R
@@ -60,6 +60,7 @@ regex_escape <- function(x, double_backslash = FALSE) {
   x <- gsub("\\{", "\\\\{", x)
   x <- gsub("\\}", "\\\\}", x)
   x <- gsub("\\*", "\\\\*", x)
+  x <- gsub("\\+", "\\\\+", x)
   return(x)
 }
 
