8c8ac8b181c53c2cb0e517d2d13811dc2347d7e2
Hao Zhu <haozhu233@gmail.com>
2018-01-23 23:45:13 -0500

    Fix the stackoverflow issue

--- R/print.R
+++ R/print.R
@@ -1,13 +1,15 @@
 #' @export
 print.kableExtra <- function(x, ...) {
-  cat(as.character(x))
   html_header <- htmltools::tags$head(
     rmarkdown::html_dependency_jquery(),
     rmarkdown::html_dependency_bootstrap(theme = "simplex"),
     html_dependency_kePrint()
   )
   html_table <- htmltools::HTML(as.character(x))
-  htmltools::html_print(htmltools::tagList(html_header, html_table))
+  html_result <- htmltools::tagList(html_header, html_table)
+  htmltools::html_print(html_result)
+
+  print(html_result)
 }
 
 html_dependency_kePrint <- function() {
