904c0dc36a6e43cef2bc121fde55988c6beaae5d
Hao Zhu <haozhu233@gmail.com>
2018-04-04 16:14:32 -0400

    Fix add_footnote

--- R/add_footnote.R
+++ R/add_footnote.R
@@ -101,7 +101,7 @@ add_footnote <- function(input, label = NULL,
     }
     label <- gsub("\\\\", "\\\\\\\\", label)
 
-    export <- solve_enc(export)
+    export <- enc2utf8(export)
     table_info <- magic_mirror(input)
     if (table_info$tabular == "longtable") {
       if (notation != "number") {
