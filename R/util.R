5d53dadc1b5ecd5bdec499ef70da3d46d582b2c7
Hao Zhu <haozhu233@gmail.com>
2018-05-20 19:02:53 -0400

    #195

--- R/util.R
+++ R/util.R
@@ -63,6 +63,7 @@ regex_escape <- function(x, double_backslash = FALSE) {
   x <- gsub("\\+", "\\\\+", x)
   x <- gsub("\\?", "\\\\?", x)
   x <- gsub("\\|", "\\\\|", x)
+  x <- gsub("\\^", "\\\\^", x)
   return(x)
 }
 
