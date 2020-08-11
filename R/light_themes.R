9bf19981f852d360572761222e3ca8b93fbd6831
Hao Zhu <haozhu233@gmail.com>
2020-08-11 00:50:33 -0400

    Adding material dark

--- R/light_themes.R
+++ R/light_themes.R
@@ -3,8 +3,8 @@
 #' @description kableExtra uses the built-in bootstrap themes by default in
 #' `kable_styling()`. Alternatively, you can use a customized table themes for
 #' your table. This `lightable` table style sheet comes with three formats,
-#' namely `lightable-minimal`, `lightable-classic` and `lightable-material` with
-#' `hover` and `striped` options.
+#' namely `lightable-minimal`, `lightable-classic`, `lightable-material` and
+#' `lightable-material-dark` with `hover` and `striped` options.
 #'
 #' @param kable_input A HTML kable object.
 #' @param lightable_options Options to customize lightable. Similar with
@@ -27,6 +27,12 @@ kable_minimal <- function(kable_input, lightable_options = "basic", ...) {
 #' @export
 kable_material <- function(kable_input, lightable_options = "basic", ...) {
   kable_light(kable_input, "lightable-material", lightable_options, ...)
+}
+
+#' @rdname kable_classic
+#' @export
+kable_material_dark <- function(kable_input, lightable_options = "basic", ...) {
+  kable_light(kable_input, "lightable-material-dark", lightable_options, ...)
 }
 
 kable_light <- function(kable_input, light_class, lightable_options, ...) {
