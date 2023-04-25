54b4094ab1409d06c9e24a56e2612ff11d1e8c2b
Marc Kupietz <kupietz@ids-mannheim.de>
2023-04-25 11:57:04 +0200

    Fix output type detection in .onLoad
    Fixes R 4.3 compatibility and resolves #750

--- R/zzz.R
+++ R/zzz.R
@@ -21,7 +21,7 @@
   auto_format <- getOption("kableExtra.auto_format", default = TRUE)
   if (auto_format) auto_set_format()
   if (!is.null(rmarkdown::metadata$output) &&
-      rmarkdown::metadata$output %in% c(
+      names(rmarkdown::metadata$output)[1] %in% c(
         "ioslides_presentation", "slidy_presentation",
         "gitbook", "bookdown::gitbook", "radix_article", "radix::radix_article",
         "distill_article", "distill::distill_article"
